bb4372887fa390ac9f75b792376debcefbc7ce96
Shereef <shphw7@gmail.com>
2024-02-07 01:51:29 +0200

    [3] [Time Beats: 23.82%] [Memory Beats: 44.38%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    if (s.length == 0) return 0
+    if (s.length == 1) return 1
+    let maxCount = 0
+    let i = 0
+    let j = 0
+    let set = new Set();
+    while (j < s.length) {
+
+        while (set.has(s[j])) {
+            set.delete(s[i])
+            i++
+        }
+        set.add(s[j])
+        maxCount = Math.max(maxCount, j - i + 1)
+        j++
+    }
+
+    return maxCount
+}
